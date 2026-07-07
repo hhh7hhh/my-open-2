2e8959eea59c05c2a541ad455440d61b27d697e2
hhh7hhh <1427743621@qq.com>
2026-07-07 12:27:09 +0800

    Add adapters/__init__.py

---
+++ adapters/__init__.py
@@ -0,0 +1,5 @@
+from .base_adapter import BaseAdapter, TaskDef, TaskResult, TaskStatus
+from .okww_adapter import OkwwAdapter
+from .m7a_adapter import M7aAdapter
+
+__all__ = ["BaseAdapter", "TaskDef", "TaskResult", "TaskStatus", "OkwwAdapter", "M7aAdapter"]
